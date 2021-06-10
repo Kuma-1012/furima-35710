456c52227ab1220693e46625eb2aef07aad94157
Kuma-1012 <yutakuma121017@icloud.com>
2021-06-10 13:16:53 +0900

    遷移機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -24,11 +25,19 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+      if current_user != @item.user
+       redirect_to root_path
+      end
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
 
@@ -38,4 +47,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :item_name, :item_info, :category_id, :item_status_id, :fee_status_id,
                                  :item_prefecture_id, :day_id, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
 end
