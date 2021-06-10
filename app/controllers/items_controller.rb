4a5b0b9a082d33e919232e3f0dce3fd4c200150a
Kuma-1012 <yutakuma121017@icloud.com>
2021-06-10 14:24:18 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update, :show]
+  before_action :return_item, only: [:edit, :update]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -20,18 +21,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path if current_user != @item.user
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
@@ -40,12 +36,16 @@ class ItemsController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def return_item
+    redirect_to root_path if current_user != @item.user
+  end
+
   def item_params
     params.require(:item).permit(:image, :item_name, :item_info, :category_id, :item_status_id, :fee_status_id,
                                  :item_prefecture_id, :day_id, :price).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    redirect_to new_user_session_path unless user_signed_in?
   end
 end
