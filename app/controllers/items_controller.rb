e1d910f4b712024b0e645b4e7268fc7180e37b57
Kuma-1012 <yutakuma121017@icloud.com>
2021-06-10 13:20:07 +0900

    Rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,9 +25,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-      if current_user != @item.user
-       redirect_to root_path
-      end
+    redirect_to root_path if current_user != @item.user
   end
 
   def update
@@ -40,7 +38,6 @@ class ItemsController < ApplicationController
     end
   end
 
-
   private
 
   def item_params
@@ -51,5 +48,4 @@ class ItemsController < ApplicationController
   def move_to_index
     redirect_to new_user_session_path unless user_signed_in?
   end
-
 end
